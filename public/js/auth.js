b1cb8188af6dffa7d81f525a89a69d6bfac2e9d7
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2022-11-16 09:50:49 +0100

    login toggler

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const toggler = document.querySelectorAll(".toggle-password2");
+const logintoggler = document.querySelector(".toggle-password");
 const password = document.querySelector("#password");
 const conpassword = document.querySelector("#conpassword");
-// console.log(toggler, location.pathname);
 
 toggler.forEach((t) => {
   t.addEventListener("click", (e) => {
@@ -20,3 +20,15 @@ toggler.forEach((t) => {
     }
   });
 });
+
+logintoggler.addEventListener("click", (e) => {
+    if (password.type === "text") {
+      logintoggler.classList =
+        "fa fa-fw fa-eye-slash text-light fs-16 field-icon toggle-password2";
+      password.type = "password";
+    } else if (password.type === "password") {
+      logintoggler.classList =
+        "fa fa-fw fa-eye text-light fs-16 field-icon toggle-password2";
+      password.type = "text";
+    }
+  });
