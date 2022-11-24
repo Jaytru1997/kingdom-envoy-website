8acfa1da30b08d56309e374cb9710d384d18ab83
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2022-11-24 01:45:08 +0100

    error handle page on async

--- utilities/async.js
+++ utilities/async.js
@@ -3,6 +3,11 @@ exports.asyncWrapper = (fn) => {
     try {
       await fn(req, res, next);
     } catch (err) {
+      res.render("../views/components/error.ejs", {error:{
+        message: "Something happend, we are unable to process your request at this moment. Kindly click on the button below to restart the process.",
+        cta: "Go back",
+        link: "/login"
+      }});
       next(err);
     }
   };
